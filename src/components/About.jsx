a46e5d17ba9614ac096e1126032d2426f8ce4899
0abilash <mailtoabilashy@gmail.com>
2026-07-25 22:01:46 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ export default function About() {
                 <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
                 <circle cx="12" cy="10" r="3" />
               </svg>
-              <span>Bangalore, India</span>
+              <span>Coimbatore, India</span>
             </div>
           </div>
 
